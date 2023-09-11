9796a5e82b29d5da9a14a80aa9dc5cfefe53949f
Grigory <gregoridagmo@gmail.com>
2023-09-11 15:03:09 +0400

    Property-DI-loggers update

--- src/main/java/com/yet/spring/core/App.java
+++ src/main/java/com/yet/spring/core/App.java
@@ -2,25 +2,39 @@ package com.yet.spring.core;
 
 import com.yet.spring.core.beans.Client;
 import com.yet.spring.core.beans.Event;
+import com.yet.spring.core.loggers.CacheFileEventLogger;
 import com.yet.spring.core.loggers.EventLogger;
+import com.yet.spring.core.loggers.EventType;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class App {
     private Client client;
     private EventLogger eventLogger;
+    private Map<EventType, EventLogger> loggers;
+    private CacheFileEventLogger defaultLogger;
 
-    public App(Client client, EventLogger eventLogger) {
-        this.client = client;
-        this.eventLogger = eventLogger;
+    public void setDefaultLogger(CacheFileEventLogger defaultLogger) {
+        this.defaultLogger = defaultLogger;
     }
 
-    private void logEvent(Event event, String msg) throws IOException {
+    public App(Client client, EventLogger eventLogger, Map<EventType, EventLogger> loggers) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+        this.loggers = loggers;
+    }
+
+    private void logEvent(Event event, String msg, EventType type) throws IOException {
         String message = msg.replaceAll(client.getId(), client.getFullName());
         event.setMsg(message);
-        eventLogger.logEvent(event);
+        EventLogger logger = loggers.get(type);
+        if (logger == null) {
+            logger = defaultLogger;
+        }
+        logger.logEvent(event);
     }
 
     public static void main(String[] args) throws IOException {
@@ -29,10 +43,13 @@ public class App {
         App app = ctx.getBean(App.class);
 
         Event event = ctx.getBean(Event.class);
-        app.logEvent(event, "User 1 joined");
+        app.logEvent(event, "User 1 joined", EventType.INFO);
 
         event = ctx.getBean(Event.class);
-        app.logEvent(event, "User 2 joined");
+        app.logEvent(event, "User 2 joined", EventType.ERROR);
+
+        event = ctx.getBean(Event.class);
+        app.logEvent(event, "User 3 joined", null);
 
         ctx.close();
     }
